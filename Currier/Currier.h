9f17b5208310a1475af99c18d8258ced7ec82767
Liam <l.flynn2@live.co.uk>
2017-02-14 08:55:03 +0000

    Initial Commit

---
+++ Currier/Currier.h
@@ -0,0 +1,19 @@
+//
+//  Currier.h
+//  Currier
+//
+//  Created by Liam on 14/02/2017.
+//  Copyright © 2017 Tigerpixel Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Currier.
+FOUNDATION_EXPORT double CurrierVersionNumber;
+
+//! Project version string for Currier.
+FOUNDATION_EXPORT const unsigned char CurrierVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Currier/PublicHeader.h>
+
+
