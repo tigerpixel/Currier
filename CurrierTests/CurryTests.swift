f025ec711cc6a3fdf5727e005622173da278d537
Liam <l.flynn2@live.co.uk>
2017-02-16 22:57:10 +0000

    Refactor the unit tests so that they can detect errors in the ordering of the parameters. This is done using different types for different parameters.

--- CurrierTests/CurryTests.swift
+++ CurrierTests/CurryTests.swift
@@ -11,9 +11,7 @@ import XCTest
 
 class CurryTests: XCTestCase {
     
-    struct ValueType {
-        let value: Float
-    }
+    // MARK: Helper types.
     
     class ReferenceType {
         
@@ -25,7 +23,29 @@ class CurryTests: XCTestCase {
         
     }
     
+    struct ValueType {
+        
+        let value: Float
+        
+    }
+    
+    // MARK: Helper functions.
+    
+    func digitSum(of value: Int) -> Int {
+        return (0...value).reduce(0){ $0 + $1 }
+    }
+    
     // MARK: Two parameters with basic types.
+    
+    func testDigitSumHelperFunction() {
+        
+        XCTAssertEqual(0, digitSum(of:0))
+        XCTAssertEqual(1, digitSum(of:1))
+        XCTAssertEqual(3, digitSum(of:2))
+        XCTAssertEqual(6, digitSum(of:3))
+        XCTAssertEqual(55, digitSum(of:10))
+        XCTAssertEqual(153, digitSum(of:17))
+    }
     
     func testTwoStringParams() {
         
@@ -55,19 +75,6 @@ class CurryTests: XCTestCase {
     
     // MARK: Two parameters intermediate types.
     
-    func testTwoStructParams() {
-        
-        func twoStructs(first: ValueType, second: ValueType) -> ValueType {
-            return ValueType(value:first.value + second.value)
-        }
-        
-        let curried = curry(twoStructs)
-        let partiallyApplied = curried(ValueType(value:3))
-        let result = partiallyApplied(ValueType(value:6))
-        
-        XCTAssertEqual(9, result.value)
-    }
-    
     func testTwoRefTypeParams() {
         
         func twoReferenceTypes(first: ReferenceType, second: ReferenceType) -> ReferenceType {
@@ -77,6 +84,19 @@ class CurryTests: XCTestCase {
         let curried = curry(twoReferenceTypes)
         let partiallyApplied = curried(ReferenceType(value:3))
         let result = partiallyApplied(ReferenceType(value:6))
+        
+        XCTAssertEqual(9, result.value)
+    }
+    
+    func testTwoStructParams() {
+        
+        func twoStructs(first: ValueType, second: ValueType) -> ValueType {
+            return ValueType(value:first.value + second.value)
+        }
+        
+        let curried = curry(twoStructs)
+        let partiallyApplied = curried(ValueType(value:3))
+        let result = partiallyApplied(ValueType(value:6))
         
         XCTAssertEqual(9, result.value)
     }
@@ -98,42 +118,56 @@ class CurryTests: XCTestCase {
         XCTAssertEqual("beta", result)
     }
     
-    // MARK: Increasing numbers of parameters.
-    
-    func testThreeParams() {
+    func testTwoDistinctParams() {
         
-        func three(first: String, second: String, third: Int) -> Int {
-            return first.characters.count + second.characters.count + third
+        func three(first: String, second: Int) -> Int {
+            return first.characters.count + second
         }
         
         let curried = curry(three)
-        let result = curried("a")("Ab")(3)
+        let result = curried("a")(2)
         
-        XCTAssertEqual(6, result)
+        XCTAssertEqual(digitSum(of:2), result)
+    }
+    
+    // MARK: Increasing numbers of parameters.
+    
+    // Parameter types differ so that changes in order can be detected while testing multiple types.
+    
+    func testThreeParams() {
+        
+        func three(first: String, second: Int, third: Float) -> Int {
+            return first.characters.count + second + Int(third)
+        }
+        
+        let curried = curry(three)
+        let result = curried("a")(2)(3.0)
+        
+        XCTAssertEqual(digitSum(of:3), result)
     }
     
     func testFourParams() {
         
-        func four(first: String, second: String, third: Int, fourth: Int) -> Int {
-            return first.characters.count + second.characters.count + third + fourth
+        func four(first: String, second: Int, third: Float, fourth: ReferenceType) -> Int {
+            return first.characters.count + second + Int(third) + Int(fourth.value)
         }
         
         let curried = curry(four)
-        let result = curried("a")("Ab")(3)(4)
+        let result = curried("a")(2)(3.0)(ReferenceType(value:4.0))
         
-        XCTAssertEqual(10, result)
+        XCTAssertEqual(digitSum(of:4), result)
     }
     
     func testFiveParams() {
         
-        func five(first: String, second: String, third: Int, fourth: Int, fifth: ValueType) -> Int {
-            return first.characters.count + second.characters.count + third + fourth + Int(fifth.value)
+        func five(first: String, second: Int, third: Float, fourth: ReferenceType, fifth: ValueType) -> Int {
+            return first.characters.count + second + Int(third) + Int(fourth.value) + Int(fifth.value)
         }
         
         let curried = curry(five)
-        let result = curried("a")("Ab")(3)(4)(ValueType(value: 5))
+        let result = curried("a")(2)(3.0)(ReferenceType(value:4.0))(ValueType(value: 5))
             
-        XCTAssertEqual(15, result)
+        XCTAssertEqual(digitSum(of:5), result)
     }
     
 }
