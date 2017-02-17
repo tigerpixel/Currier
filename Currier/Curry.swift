645d7858b2658fea96eb0346d8f422e0ce153a56
Liam <l.flynn2@live.co.uk>
2017-02-17 17:03:29 +0000

    Refactor spacing on Twelve parameter curry function.

--- Currier/Curry.swift
+++ Currier/Curry.swift
@@ -162,11 +162,16 @@ public func curry<First, Second, Third, Fourth, Fifth, Sixth,
     }
 }
 
-public func curry<First, Second, Third, Fourth, Fifth, Sixth,
-                  Seventh, Eighth, Ninth, Tenth, Eleventh, Twelfth, Result>(_ uncurried:
-    @escaping (First, Second, Third, Fourth, Fifth, Sixth, Seventh, Eighth, Ninth, Tenth, Eleventh, Twelfth) -> Result)
-    -> (First) -> (Second) -> (Third) -> (Fourth) -> (Fifth) ->
-    (Sixth) -> (Seventh) -> (Eighth) -> (Ninth) -> (Tenth) -> (Eleventh) -> (Twelfth) -> Result {
+public func curry<First, Second, Third, Fourth,
+                  Fifth, Sixth, Seventh, Eighth,
+                  Ninth, Tenth, Eleventh, Twelfth, Result>(_ uncurried:
+
+    @escaping (First, Second, Third, Fourth,
+               Fifth, Sixth, Seventh, Eighth,
+               Ninth, Tenth, Eleventh, Twelfth) -> Result)
+    -> (First) -> (Second) -> (Third) -> (Fourth)
+    -> (Fifth) -> (Sixth) -> (Seventh) -> (Eighth)
+    -> (Ninth) -> (Tenth) -> (Eleventh) -> (Twelfth) -> Result {
 
         return { first in { second in { third in { fourth
             in { fifth in { sixth in { seventh in { eighth
