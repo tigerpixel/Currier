bb5bafaf7cd46fa9c757ddbe7977284d033ecb9c
Liam <l.flynn2@live.co.uk>
2017-03-02 09:00:40 +0000

    Add a target for watchOS.
    Add a target for watchOS.
    
    Use the minimum deployment target.
    
    Remove the README from the iOS target.

---
+++ Currier-watchOS/Currier-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  Currier-watchOS.h
+//  Currier-watchOS
+//
+//  Created by Liam on 02/03/2017.
+//  Copyright © 2017 Tigerpixel Ltd. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Currier-watchOS.
+FOUNDATION_EXPORT double Currier_watchOSVersionNumber;
+
+//! Project version string for Currier-watchOS.
+FOUNDATION_EXPORT const unsigned char Currier_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Currier_watchOS/PublicHeader.h>
+
+
