0724456faed9e090d95ee784dc5da1e0ff7fa9c5
Liam <l.flynn2@live.co.uk>
2017-03-14 00:17:21 +0000

    Missed files on previous commit.

--- Package.swift
+++ Package.swift
@@ -6,6 +6,8 @@
 //  Copyright © 2017 Tigerpixel Ltd. All rights reserved.
 //
 
+/// A Currier Package Description for use with the Swift Package Manager
+
 import PackageDescription
 
 let package = Package(
