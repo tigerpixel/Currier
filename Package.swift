40c00af57c3e018450b3a86ef4520cda8ea9997a
Liam <l.flynn2@live.co.uk>
2017-03-14 00:17:03 +0000

    Add Package file then arrange the project to meet SPM layout requirements.
    Add a Package.swift file.
    Move code into Sources directory.
    Move unit tests into Tests directory.
    Update file locations.
    Update podspec.
    All targets now build in xocde, xcodebuild and SPM locally.

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  Currier
+//
+//  Created by Liam on 09/03/2017.
+//  Copyright © 2017 Tigerpixel Ltd. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "Currier"
+)
